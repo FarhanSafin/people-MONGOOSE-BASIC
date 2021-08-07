92846b6bb3f79cd3e3969381413e6ea7e1f3065c
FarhanSafin <52666674+FarhanSafin@users.noreply.github.com>
2021-08-07 10:43:33 +0600

    Mongoose CRUD-ALL

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ const mongoose = require ("mongoose");
 mongoose.connect("mongodb://localhost:27017/peopleDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 //schemaCreate
+//adding validator...w/o name cannt entry..age limitation
 const peopleSchema = new mongoose.Schema({
-    name: String,
-    age: Number
+    name: {
+        type: String,
+        required: [true, "Please enter a name!"]
+    },
+    age: {
+        type: Number,
+        min: 0,
+        max: 100
+    }
 });
 
 //modelCreate
@@ -25,7 +33,9 @@ const captain = new People({
     name: "Captain",
     age: 34
 });
-//saving data one by one ----- "johnson.save();"
+
+
+/* //saving data one by one ----- "johnson.save();"
 //saving in bulk(many)
 People.insertMany([johnson, hulk, captain], function(err){
     if (err) {
@@ -34,4 +44,60 @@ People.insertMany([johnson, hulk, captain], function(err){
     else {
         console.log("Success");
     }
+}); */
+
+
+//finding/reading all data from database
+People.find(function(err, peoples){
+    if(err){
+        console.log(err);
+    } else{
+        console.log(peoples);
+    }
+});
+
+
+//finding/reading specific data from database
+People.find(function(err, peoples){
+    if(err){
+        console.log(err);
+    } else{
+
+
+        //closing mongoose database connection
+        mongoose.connection.close();
+
+
+        peoples.forEach(function(people){
+            console.log(people.name);
+        })
+    }
+});
+
+//Updating DATA
+People.updateOne({_id:"610e007f69088b24a887d233"}, {name:"Salim"}, function(err){
+    if(err){
+        console.log(err);
+    }else{
+        console.log("Updated");
+    }
 })
+
+//deleting data
+People.deleteOne({name: "Johnson"}, function(err){
+    if(err){
+        console.log(err)
+    }else{
+        console.log("Deleted successfully");
+    }
+});
+
+
+//delete Many
+People.deleteMany({name: "Hulk"}, function(err){
+    if(err){
+        console.log(err);
+    } else{
+        console.log("Successfully deleted all HULK");
+    }
+});
